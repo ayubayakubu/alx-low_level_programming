ea1857219ca3c85bcbf6026cbee3e8254520fdd9
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-13 18:25:24 -0700

    Writing a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,12 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of a int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of a short int: %2d byte(s)\n", sizeof(short int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)\n", sizeof(float));
+	printf("Size of a double: %2d byte(s)\n", sizeof(double));
+	printf("Size of a long double: byte(s)\n", sizeof(long double));
 	return (0);
 }
