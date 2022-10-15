07963e4035b92a3f62148b949f3256742204e30b
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-14 20:28:46 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 int main(void)
 
 {
-	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZ";
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
 	{
-		putchar(a(i));
+		putchar(alpha(i));
 	putchar('\n');
 	return (0);
 	}
