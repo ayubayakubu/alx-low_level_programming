d2da71afed63f4ed2cba836b6639f7520cfb2f3d
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-15 19:19:25 -0700

    Exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Displaying alphabets excluding some
+ * Return:Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
