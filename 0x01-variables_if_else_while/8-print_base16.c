05ff9cfde89e0b10e5af9e932329de2b5af7e07f
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-16 19:52:19 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - printing hexadecimal digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char l;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0')
+	for (l = 'a'; l <= 'f'; l++)
+
+	putchar("\n");
+
+	return (0);
+}
