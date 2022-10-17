eec04ae6a67e2a1d071023b1af491bbc86e9f947
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-16 19:24:55 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main  -printing Lower cade alphabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= a; l--)
+		putchar(l);
+	putchar("\n");
+	return (0);
+}
