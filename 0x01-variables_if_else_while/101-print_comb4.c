ecf3ce72f304cee292da6c808113db7e71fe3f8c
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-17 17:48:20 -0700

    Three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 0;
+	while (i < 1000)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 100;
+
+		if (j < k && k < l)
+		{
+		putchar(l + '0');
+		putchar(k + '0');
+		purchar(j + '0');
+		if (i < 789)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
