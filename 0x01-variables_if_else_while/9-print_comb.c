a82fe742fe24cd8adcb257f1dc190d9c6722a5d4
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-17 18:59:51 -0700

    all

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,9 +7,10 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
-		putchar(n + '0');
-		if (n != 9)
+	for (n = 0; n <= 9; n++)
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
 		{
 			putchar(',');
 			putchar(' ');
