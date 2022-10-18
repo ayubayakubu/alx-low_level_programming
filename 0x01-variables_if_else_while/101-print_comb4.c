4c9d8b29affb431f746e73c7533f49ef9ef5d63e
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-17 18:21:29 -0700

    another

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,14 @@
  */
 int main(void)
 {
+	int i, j, k;
 	char digit[4];
 
-	for (int i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (int j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-			for (int k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				digit[0] = i + '0';
 				digit[2] = j + '0';
