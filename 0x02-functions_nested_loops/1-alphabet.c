24b46159b21451925705ad9fc5927099b950a71c
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-18 20:58:07 -0700

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+/**
+ * main - printing alphabets
+ * Return:Always 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	return (0);
+	_putchar('\n');
+}
+
