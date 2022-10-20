a8c831c9ff91a0eaf250c5c4c83a2d4a6b821a76
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-20 16:58:48 -0700

    third

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul- mulyiple two integet
+ * @a: first integer
+ * @b: second integer
+ * Return: the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
