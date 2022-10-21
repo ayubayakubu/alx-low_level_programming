dfaa0d2c06587ef50f20f22073ee925d77d64df2
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-20 17:19:03 -0700

    fifth

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbet from 0 to 9
+ * without 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int b = 0;
+
+	while (b < 10)
+	{
+		if (b != 2 && b != 4)
+			_putchat(a + '0');
+		a++;
+	}
+	_putchar('\n');
+}
+
