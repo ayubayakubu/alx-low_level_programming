3b3a4f7b51b167ae05435e04c15681202118e8e6
Yakubu Ayuba <ayubayakub@yahoo.com>
2022-10-25 16:24:53 -0700

    second

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int- swap one int ti another
+ * @a: int a
+ * @b: int b
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+
+	int a = *b;
+	int *b = *c;
+	int *c = a;
+
+}
+
